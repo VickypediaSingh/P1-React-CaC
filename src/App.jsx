58afd1dfe8da0c3587c554b4e44eb9b7c8738a1f
Bikramjeet Singh <officialbikramjeet@gmail.com>
2024-06-02 14:07:23 +0530

    nothing much just revising

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from "react";
 
+const colors = ["red", "green", "blue"];
+
 function App() {
-  const [color, setColor] = useState("olive");
+  const [bgColor, setBgColor] = useState("whitesmoke");
+
   return (
     <div
-      className="w-full h-screen duration-200"
-      style={{ backgroundColor: color }}
+      className="w-full h-screen duration-500"
+      style={{ backgroundColor: bgColor }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button
-            onClick={() => setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "red" }}
-          >
-            Red
-          </button>
-          <button
-            onClick={() => setColor("green")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "green" }}
-          >
-            Green
-          </button>
-          <button
-            onClick={() => setColor("blue")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "blue" }}
-          >
-            Blue
-          </button>
+          {colors.map((color) => (
+            <button
+              key={color} // Important for accessibility and performance
+              onClick={() => setBgColor(color)}
+              className={`outline-none px-4 py-1 rounded-full text-black shadow-lg bg-${color}-500`}
+              style={{ backgroundColor: color }} // Set background color directly
+            >
+              {color}
+            </button>
+          ))}
         </div>
       </div>
     </div>
